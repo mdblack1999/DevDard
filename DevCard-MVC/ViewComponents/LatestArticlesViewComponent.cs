232b9ddb897b322e3aaba778049d21a873162a5e
mdblack <uac78@yahoo.com>
2025-03-06 18:58:58 +0330

    Footeer & Header Moved To Partial View

---
+++ DevCard-MVC/ViewComponents/LatestArticlesViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_MVC.ViewComponents
+{
+    public class LatestArticlesViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var article = new List<Article>
+            {
+                new Article(1,"آموزش طراحی سایت حرفه ای و پیشرفته","کامل ترین دوره طراحی سایت و ورود به بازار کار با Asp.Net Core MVC","blog-post-thumb-card-1.jpg"),
+                new Article(2,"آموزش جامع javaScript","تنها دوره جامع و فارسی زبان آموزش JavaScript مقدماتی و پیشرفته تا ورود به بازار کار","blog-post-thumb-card-2.jpg"),
+                new Article(3,"آموزش Html و css حرفه ای","کامل ترین دوره طراحی سایت و ورود به بازار کار با Html,Css","blog-post-thumb-card-3.jpg"),
+                new Article(4,"آموزش زبان برنامه نویسی C# مقدماتی","دوره مقدماتی سی شارپ","blog-post-thumb-card-4.jpg"),
+                new Article(5,"آموزش زبان برنامه نویسی C# پیشرفته","دوره پیشرفته سی شارپ","blog-post-thumb-card-5.jpg"),
+            };
+            return View("_LatestArticles",article);
+        }
+    }
+}
---
+++ DevCard-MVC/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_MVC.ViewComponents
+{
+	public class ProjectsViewComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			var projects = new List<Project>
+			{
+				new Project(1, "تاکسی اینترنتی", "درخواست آنلاین تاکسی برای سفر های درون شهری","project-1.jpg", "MDCO"),
+				new Project(1, "زود فود کده", "درخواست آنلاین و سریع غذای سنتی و آماده همراه با مخلفات", "project-6.jpg","zoodFood.Co"),
+				new Project(1, "قهوه  آل ایچ", "سرو و ارسال انواع قهوه و دسر و نوشیدنی های گرم و سرد","project-7.jpg", "Alleach"),
+				new Project(1, "پوشاک مایا", "ارسال انواع لباس زمستانی و تابستانی مردانه و زنانه در طرح های مختلف","project-4.jpg",
+                    "maya")
+			};
+			return View("_Projects" , projects);
+		}
+	}
+}
